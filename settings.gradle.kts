146be94d0017864bd24068f95ccb882650902012
Yash Kalariya <yash.k@ixfi.com>
2024-01-31 17:21:12 +0530

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+rootProject.name = "cmmLibrary"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+include(":sample:composeApp")
+include(":kmmchart")
+pluginManagement {
+    repositories {
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
+include(":composeApp")
